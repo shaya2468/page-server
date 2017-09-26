e311bf58924af315281866eb2cd2f2f36486955c
shaya246 <shayasms128@gmail.com>
2017-09-26 20:22:49 +0300

    file for really basic stuff

---
+++ playground/littlethings.js
@@ -0,0 +1,13 @@
+var URL='www.google.co.il';
+
+var arr=URL.split('/')
+
+
+
+console.log(arr);
+
+
+// var parameter=arr[arr.length-1].split('?');//parameter[0]='14aD9Uxp'
+//                                            //parameter[1]='p=10'
+
+// var p_value=parameter[1].split('=')[1];//p_value='10';
