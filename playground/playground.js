d865e1562908e5afe24221b2c37e5a1e4f2a7f10
shaya246 <shayasms128@gmail.com>
2017-09-26 14:20:21 +0300

    asdf

--- playground/playground.js
+++ playground/playground.js
@@ -31,9 +31,9 @@ var postStuff = function(){
 }
 
 
-// postStuff();
 
-var getStuff = function(){
+
+var getStuff = function(query){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {//Call a function when the state changes.
 
@@ -44,10 +44,39 @@ var getStuff = function(){
         }
     }
 
+    var userString = query.user_name ? 'user_name=' + query.user_name : "";
+    var siteString = query.site_name ? 'site_name=' + query.site_name : "";
+    var bothDefined = userString && siteString ? "&" : "";
+    
+    var finalQuery = userString + bothDefined + siteString;
 
-    var queryThing = 'site_name=walla';
-    xhttp.open("GET", "http://localhost:3333/entries?" + queryThing, true);
+    xhttp.open("GET", "http://localhost:3333/entries?" + finalQuery, true);
     xhttp.send();
 }
 
-getStuff();
+var getMyIp = function(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {//Call a function when the state changes.
+
+        if(xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log( JSON.parse(xhttp.responseText).ip );
+
+
+
+        }else{
+            // console.log('what?! ' + xhttp.status );
+        }
+    }
+
+
+    xhttp.open("GET", "https://freegeoip.net/json/", true);
+    xhttp.send();
+}
+
+
+
+
+getStuff({site_name:"walla"});
+getStuff({user_name:"eric"});
+// postStuff();
+// getMyIp();
