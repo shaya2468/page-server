3f336e8abcfe9359525a78f1b415ca52cc592f09
shaya246 <shayasms128@gmail.com>
2017-09-26 12:24:53 +0300

    added get for users

--- playground/playground.js
+++ playground/playground.js
@@ -1,26 +1,50 @@
 // Form fields, see IDs above
+
+
+
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const params = {
-    user_name: 'eric',
-    site_name: 'ynet',
-    time: 666333,
-    referrer: "google"
-
-}
-
-const http = new XMLHttpRequest();
-http.open('POST', 'http://localhost:3333/entries', false);
-http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
-console.log('aaa');
-http.onreadystatechange = function() {//Call a function when the state changes.
-    console.log('bbb');
-    if(http.readyState == 4 && http.status == 200) {
-        console.log(http.responseText);
-    }else{
-        console.log('what?! ' + http.status );
+var postStuff = function(){
+    
+    const params = {
+        user_name: 'kyle',
+        site_name: 'walla',
+        time: 666333,
+        referrer: "google"
+    
     }
+    
+    const http = new XMLHttpRequest();
+    http.open('POST', 'http://localhost:3333/entries', false);
+    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    
+    console.log('aaa');
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        console.log('bbb');
+        if(http.readyState == 4 && http.status == 200) {
+            console.log(http.responseText);
+        }else{
+            console.log('what?! ' + http.status );
+        }
+    }
+    
+    http.send(JSON.stringify(params)) // Make sure to stringify
 }
 
-http.send(JSON.stringify(params)) // Make sure to stringify
 
+// postStuff();
+
+var getStuff = function(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {//Call a function when the state changes.
+
+        if(xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log(xhttp.responseText);
+        }else{
+            // console.log('what?! ' + xhttp.status );
+        }
+    }
+    xhttp.open("GET", "http://localhost:3333/entries/kyle?aaa=bbb&blbbbb=pppp", true);
+    xhttp.send();
+}
+
+getStuff();
--- server/controllers/entry_controller.js
+++ server/controllers/entry_controller.js
@@ -5,8 +5,6 @@ module.exports = {
 
   add(req, res) {
     var body = _.pick(req.body, ['user_name', 'site_name', 'time', 'referrer']);
-    console.log(body);
-    console.log(req.body);
     var entry = new Entry(body);
 
     entry
@@ -15,9 +13,23 @@ module.exports = {
         res.send(doc);
     }, 
     (e) => {
-        // console.log(e);
         res.status(400).send(e);
     });
    },
+
+   getByName(req, res){
+    var name = req.params.id;
+    console.log('params is ' + JSON.stringify(req.params));
+    console.log('query is ' + JSON.stringify(req.query));
+
+    Entry.find({
+      user_name: name
+    })
+    .then((docs) => {
+      res.send(docs);
+    }).catch((e) => {
+      res.status(400).send(e);
+    });
+  },
 
 };
